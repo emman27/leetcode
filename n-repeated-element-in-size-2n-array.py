772c40b5af311fe7a6ee096b70f921839e06a09e
Emmanuel Goh <emmanuel@visenze.com>
2019-08-19 17:44:50 +0800

    feat(n-repeated-element-in-size-2n-array.py):

---
+++ n-repeated-element-in-size-2n-array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def repeatedNTimes(self, A: List[int]) -> int:
+        found = {}
+        for elem in A:
+            if found.get(elem, 0) != 0:
+                return elem
+            found[elem] = 1
+        return -1
