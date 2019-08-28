3ea5d02f41f28ce47a4c0fd9672852665c011e72
Emmanuel Goh <emmanuel@visenze.com>
2019-08-28 14:22:05 +0800

    feat(height-checker.py):

---
+++ height-checker.py
@@ -0,0 +1,8 @@
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        sorted_heights = sorted(heights)
+        count = 0
+        for left, right in zip(sorted_heights, heights):
+            if left != right:
+                count += 1
+        return count
