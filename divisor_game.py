47362b7870c79cb774ac8aef71d4a0d060c28fd2
Emmanuel Goh <emmanuel@visenze.com>
2019-09-16 20:57:02 +0800

    refactor: 💡 Divisor game
    There's a mathematical induction proof of this being true (wow!)
    
    Signed-off-by: Emmanuel Goh <emmanuel@visenze.com>

--- divisor_game.py
+++ divisor_game.py
@@ -1,8 +1,3 @@
 class Solution:
     def divisorGame(self, N: int) -> bool:
-        if N == 1:
-            return False
-        elif N == 2:
-            return True
-        else:
-            return not self.divisorGame(N-1)
+        return N % 2 == 0
