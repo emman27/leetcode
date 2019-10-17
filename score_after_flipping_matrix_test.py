5972cc7d52397b75a826a88f6eff39c407735ab8
Emmanuel Goh <emmanuel@visenze.com>
2019-10-18 00:07:50 +0800

    test: 💍 Add test that flips rows
    Signed-off-by: Emmanuel Goh <emmanuel@visenze.com>

--- score_after_flipping_matrix_test.py
+++ score_after_flipping_matrix_test.py
@@ -4,3 +4,10 @@ from score_after_flipping_matrix import Solution
 def test_already_correct():
     assert 1 == Solution().matrixScore([[1]])
     assert 6 == Solution().matrixScore([[1, 1], [1, 1]])
+
+
+def test_flips_rows_that_start_with_zero():
+    assert 1 == Solution().matrixScore([[0]])
+    assert 4 == Solution().matrixScore([[0, 1], [0, 1]])
+    assert 4 == Solution().matrixScore([[0, 1], [1, 0]])
+    assert 6 == Solution().matrixScore([[0, 0], [0, 0]])
