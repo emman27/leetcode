1debd7ef460898487c4fcdfd1faeafbc9979568f
Emmanuel Goh <emmanuel@visenze.com>
2019-10-18 00:13:25 +0800

    test: 💍 Add column flipping test
    Signed-off-by: Emmanuel Goh <emmanuel@visenze.com>

--- score_after_flipping_matrix_test.py
+++ score_after_flipping_matrix_test.py
@@ -8,6 +8,11 @@ def test_already_correct():
 
 def test_flips_rows_that_start_with_zero():
     assert 1 == Solution().matrixScore([[0]])
-    assert 4 == Solution().matrixScore([[0, 1], [0, 1]])
-    assert 4 == Solution().matrixScore([[0, 1], [1, 0]])
     assert 6 == Solution().matrixScore([[0, 0], [0, 0]])
+
+
+def test_flips_columns():
+    assert 6 == Solution().matrixScore([
+        [1, 0],
+        [1, 0]
+    ])
