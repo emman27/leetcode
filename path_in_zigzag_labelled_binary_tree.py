2cdb3e72aa5933bbfe842cd5a88b05b311bd3f16
Emmanuel Goh <emmanuel@visenze.com>
2019-10-17 23:25:35 +0800

    feat: 🎸 Path in zigzag labelled binary tree
    Signed-off-by: Emmanuel Goh <emmanuel@visenze.com>

---
+++ path_in_zigzag_labelled_binary_tree.py
@@ -0,0 +1,6 @@
+from typing import List
+
+
+class Solution:
+    def pathInZigZagTree(self, label: int) -> List[int]:
+        return []
---
+++ path_in_zigzag_labelled_binary_tree_test.py
@@ -0,0 +1,5 @@
+from path_in_zigzag_labelled_binary_tree import Solution
+
+
+def test_root():
+    assert [1] == Solution().pathInZigZagTree(1)
