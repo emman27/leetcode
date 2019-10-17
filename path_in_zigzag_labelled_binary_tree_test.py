521cc9eb7edc930be68b3c28849d50087e126991
Emmanuel Goh <emmanuel@visenze.com>
2019-10-17 23:55:15 +0800

    feat: 🎸 Full-featured implementation
    Signed-off-by: Emmanuel Goh <emmanuel@visenze.com>

--- path_in_zigzag_labelled_binary_tree_test.py
+++ path_in_zigzag_labelled_binary_tree_test.py
@@ -7,3 +7,13 @@ def test_root():
 
 def test_right():
     assert [1, 2] == Solution().pathInZigZagTree(2)
+
+
+def test_left():
+    assert [1, 3] == Solution().pathInZigZagTree(3)
+
+
+def test_position_of_final_node():
+    assert 1 == Solution()._position_of_final_node(1)
+    assert 2 == Solution()._position_of_final_node(2)
+    assert 1 == Solution()._position_of_final_node(3)
