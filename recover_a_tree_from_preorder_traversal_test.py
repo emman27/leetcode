2a112b4f57d8b4a4b72374669b802c1a17375459
Emmanuel Goh <emmanuel@visenze.com>
2019-10-18 22:40:32 +0800

    test: 💍 Add test for fully filled single layer
    Signed-off-by: Emmanuel Goh <emmanuel@visenze.com>

--- recover_a_tree_from_preorder_traversal_test.py
+++ recover_a_tree_from_preorder_traversal_test.py
@@ -3,3 +3,10 @@ from recover_a_tree_from_preorder_traversal import Solution, TreeNode
 
 def test_trivial():
     assert TreeNode(1) == Solution().recoverFromPreorder("1")
+
+
+def test_single_layer():
+    solution = TreeNode(1)
+    solution.left = TreeNode(2)
+    solution.right = TreeNode(3)
+    assert solution == Solution().recoverFromPreorder("1-2-3")
