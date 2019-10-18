d35fc278387654091b94952198937ac090e4e6b1
Emmanuel Goh <emmanuel@visenze.com>
2019-10-18 22:40:44 +0800

    feat: 🎸 Add impl for fully filled single layer
    Signed-off-by: Emmanuel Goh <emmanuel@visenze.com>

--- recover_a_tree_from_preorder_traversal.py
+++ recover_a_tree_from_preorder_traversal.py
@@ -6,9 +6,16 @@ class TreeNode:
         self.right = None
 
     def __eq__(self, other):
+        if bool(self) != bool(other):
+            return False
         return self.val == other.val and self.left == other.left and self.right == other.right
 
 
 class Solution:
     def recoverFromPreorder(self, S: str) -> TreeNode:
-        return TreeNode(int(S[0]))
+        nodes = [int(val) for val in S.split('-') if val]
+        root = TreeNode(nodes[0])
+        if len(nodes) > 1:
+            root.left = TreeNode(nodes[1])
+            root.right = TreeNode(nodes[2])
+        return root
